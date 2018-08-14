f8eed23eee359ed9ca942cc366f39e4bbd7b0b46
Kus Cámara <kcmr@users.noreply.github.com>
2018-08-14 11:57:25 +0200

    test(string-utils): add tests

---
+++ test/string-utils.spec.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const stringUtils = require('../lib/string-utils');
+
+require('chai').should();
+
+describe('camelCase()', () => {
+  it('transforms to camelCase strings with words separated by - or _', () => {
+    stringUtils.camelCase('some-test_string').should.equal('someTestString');
+  });
+
+  it('does not transform strings without - or _', () => {
+    stringUtils.camelCase('notransform').should.equal('notransform');
+  });
+});
+
+describe('capitalize()', () => {
+  it('transforms to uppercase the first letter of a string', () => {
+    stringUtils.capitalize('some-string').should.equal('Some-string');
+  });
+});
+
+describe('titleCase()', () => {
+  it('transforms a string with words separated by - or _ to camelcase with the first character in uppercase', () => {
+    stringUtils.titleCase('some-test_string').should.equal('SomeTestString');
+  });
+});
