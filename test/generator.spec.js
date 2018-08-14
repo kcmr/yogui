5c97cca55185fce6da069ae20ede94a342fb08bd
Kus Cámara <kcmr@users.noreply.github.com>
2018-08-15 01:44:22 +0200

    test(generator): multiple generators and default file
    GH-3

--- test/generator.spec.js
+++ test/generator.spec.js
@@ -2,24 +2,45 @@
 
 const Generator = require('../lib/generator');
 const inquirer = require('inquirer');
-const fs = require('fs-extra');
+const {scaffold} = require('nunjucks-scaffold-generator');
 
 jest.mock('inquirer');
+jest.mock('nunjucks-scaffold-generator');
+
+const answersForGeneratorMock = {
+  name: 'any',
+  dest: 'output'
+};
 
 describe('Generator', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  beforeAll(() => {
+    process.chdir('test/fixtures');
+  });
+
   describe('calling init() without config file', () => {
-    it('throws an error', async () => {
+    it('uses .gnrtrrc file as config', () => {
+      const instance = new Generator();
+
+      inquirer.prompt.mockResolvedValue(answersForGeneratorMock);
+      
+      return instance.init()
+        .then(() => {
+          expect(instance.config.configFileName).toEqual('.gnrtrrc');
+        });
+    });
+  });
+
+  describe('calling init() with config file', () => {
+    it('throws an error if the config file is not found', async () => {
       const instance = new Generator({
         configFileName: 'config'
       });
 
       await expect(instance.init()).rejects.toThrow('No config found in a config file');
-    });
-  });
-
-  describe('calling init() with config file', () => {
-    beforeEach(() => {
-      process.chdir('test/fixtures');
     });
 
     it('skips questions for generator type if there is only one generator in config', () => {
@@ -27,18 +48,46 @@ describe('Generator', () => {
         configFileName: 'single-generator.json'
       });
 
-      const inquirerMockResponse = {
-        name: 'any',
-        dest: 'output'
-      };
-
-      inquirer.prompt.mockResolvedValue(inquirerMockResponse);
+      inquirer.prompt.mockResolvedValue(answersForGeneratorMock);
 
       return instance.init()
         .then(() => {
+          expect(inquirer.prompt).not.toBeCalledWith(instance._promptForTypes);
           expect(inquirer.prompt.mock.calls.length).toBe(1);
+        });
+    });
+
+    it('prompts for generator type if there are multiple generators in config', () => {
+      const instance = new Generator({
+        configFileName: 'multiple-generators.json'
+      });
+
+      inquirer.prompt
+        .mockResolvedValueOnce({type: 'scaffold-1'})
+        .mockResolvedValueOnce(answersForGeneratorMock);
+
+      return instance.init()
+        .then(() => {
+          expect(inquirer.prompt).toBeCalledWith(instance._promptForTypes);
         })
-        .then(() => fs.remove('output'));
+        .then(() => {
+          expect(inquirer.prompt.mock.calls.length).toBe(2);
+        });
+    });
+
+    it('the scaffold file name is not replaced if not provided in config', () => {
+      const instance = new Generator({
+        configFileName: 'multiple-generators.json'
+      });
+
+      inquirer.prompt
+        .mockResolvedValueOnce({type: 'scaffold-2'})
+        .mockResolvedValueOnce(answersForGeneratorMock);
+
+      return instance.init()
+        .then(() => {
+          expect(scaffold.mock.calls[0][0].replacement).toBeUndefined();
+        });
     });
   });
 });
