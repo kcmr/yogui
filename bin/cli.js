bd317035478596b031ab4af329a175f13da6b0ad
Kus Cámara <kcmr@users.noreply.github.com>
2018-08-14 20:37:35 +0200

    refactor(cli): log messages with colors

--- bin/cli.js
+++ bin/cli.js
@@ -4,9 +4,19 @@
 
 const Generator = require('../lib/generator');
 const log = require('fancy-log');
+const chalk = require('chalk');
 
 const cli = new Generator();
 
+const logResult = (result) => {
+  const message = `${result.generator} successfully generated in ${result.dest}`;
+  log(chalk.green(message));
+};
+
+const logError = (error) => {
+  log.error(chalk.red(error.message));
+};
+
 cli.init()
-  .then((result) => log(`${result.generator} successfully generated in ${result.dest}`))
-  .catch(error => log.error(error.message));
+  .then(logResult)
+  .catch(logError);
