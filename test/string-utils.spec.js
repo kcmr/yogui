2d53252f273fa807c3f7da4b2c48432376e08c58
Kus Cámara <kcmr@users.noreply.github.com>
2018-08-14 13:03:17 +0200

    test(string-utils): move all the tests to a describe for better output

--- test/string-utils.spec.js
+++ test/string-utils.spec.js
@@ -4,24 +4,26 @@ const stringUtils = require('../lib/string-utils');
 
 require('chai').should();
 
-describe('camelCase()', () => {
-  it('transforms to camelCase strings with words separated by - or _', () => {
-    stringUtils.camelCase('some-test_string').should.equal('someTestString');
+describe('String utils', () => {
+  describe('camelCase()', () => {
+    it('transforms to camelCase strings with words separated by - or _', () => {
+      stringUtils.camelCase('some-test_string').should.equal('someTestString');
+    });
+  
+    it('does not transform strings without - or _', () => {
+      stringUtils.camelCase('notransform').should.equal('notransform');
+    });
   });
-
-  it('does not transform strings without - or _', () => {
-    stringUtils.camelCase('notransform').should.equal('notransform');
+  
+  describe('capitalize()', () => {
+    it('transforms to uppercase the first letter of a string', () => {
+      stringUtils.capitalize('some-string').should.equal('Some-string');
+    });
   });
-});
-
-describe('capitalize()', () => {
-  it('transforms to uppercase the first letter of a string', () => {
-    stringUtils.capitalize('some-string').should.equal('Some-string');
-  });
-});
-
-describe('titleCase()', () => {
-  it('transforms a string with words separated by - or _ to camelcase with the first character in uppercase', () => {
-    stringUtils.titleCase('some-test_string').should.equal('SomeTestString');
+  
+  describe('titleCase()', () => {
+    it('transforms a string with words separated by - or _ to camelcase with the first character in uppercase', () => {
+      stringUtils.titleCase('some-test_string').should.equal('SomeTestString');
+    });
   });
 });
