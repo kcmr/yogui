d519290da5a240342ea13a88dda015ed24488980
Kus Cámara <kcmr@users.noreply.github.com>
2018-08-14 20:38:05 +0200

    style(test/string-utils): automatic lint
    GH-3

--- test/string-utils.spec.js
+++ test/string-utils.spec.js
@@ -7,18 +7,18 @@ describe('String utils', () => {
     it('transforms to camelCase strings with words separated by - or _', () => {
       expect(stringUtils.camelCase('some-test_string')).toEqual('someTestString');
     });
-  
+
     it('does not transform strings without - or _', () => {
       expect(stringUtils.camelCase('notransform')).toEqual('notransform');
     });
   });
-  
+
   describe('capitalize()', () => {
     it('transforms to uppercase the first letter of a string', () => {
       expect(stringUtils.capitalize('some-string')).toEqual('Some-string');
     });
   });
-  
+
   describe('titleCase()', () => {
     it('transforms a string with words separated by - or _ to camelcase with the first character in uppercase', () => {
       expect(stringUtils.titleCase('some-test_string')).toEqual('SomeTestString');
