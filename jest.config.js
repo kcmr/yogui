4285c9806e3ae4454d81d08bd688cc6ffee5ed3c
Kus Cámara <kcmr@users.noreply.github.com>
2018-08-14 20:38:35 +0200

    test(): add fixtures for generator
    GH-3

--- jest.config.js
+++ jest.config.js
@@ -18,4 +18,4 @@ module.exports = {
       statements: 85
     }
   }
-}
+};
--- test/generator.spec.js
+++ test/generator.spec.js
@@ -1,15 +1,36 @@
 'use strict';
 
 const Generator = require('../lib/generator');
+const inquirer = require('inquirer');
+
+jest.mock('inquirer');
 
 describe('Generator', () => {
-  describe('init()', () => {
-    it('throws an error if the config file is not found', async () => {
+  describe('calling init() without config file', () => {
+    it('throws an error', async () => {
       const instance = new Generator({
         configFileName: 'config'
       });
 
       await expect(instance.init()).rejects.toThrow('No config found in a config file');
+    });
+  });
+
+  describe('calling init() with config file', () => {
+    it('skips questions for generator type if there is only one generator in config', () => {
+      const instance = new Generator();
+
+      const inquirerMockResponse = {
+        scope: 'any',
+        name: 'any',
+        dest: 'any'
+      };
+
+      inquirer.prompt.mockResolvedValue(inquirerMockResponse);
+
+      return instance.init().then(() => {
+        expect(inquirer.prompt.mock.calls.length).toBe(1);
+      });
     });
   });
 });
